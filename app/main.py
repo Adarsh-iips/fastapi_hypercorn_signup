8f5642feab9f8afbd9447fdb2dc5fa9b7aae355c
Adarsh Sharma <147425143+Adarsh-iips@users.noreply.github.com>
2025-05-23 18:22:07 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,19 +2,26 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
-from app.routers import user, dashboard
-
+from starlette.middleware.sessions import SessionMiddleware 
+import os
 app = FastAPI()
-
+  # Add blog here
+from app.database import engine
+from app.routers import user, dashboard
+from app import models
+models.Base.metadata.create_all(bind=engine)
 # Mount static files (profile pictures)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Jinja2 templates
 templates = Jinja2Templates(directory="templates")
 
+
 # Routers
 app.include_router(user.router)
 app.include_router(dashboard.router)
+
+
 
 # CORS (optional)
 app.add_middleware(
@@ -24,3 +31,5 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.add_middleware(SessionMiddleware, secret_key=os.environ.get("SECRET_KEY", "your-very-secret-key"))
